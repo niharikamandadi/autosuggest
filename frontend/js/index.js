7acd9a5a5268822ffed5676ff49588d891a97cc2
Niharika <niharikamandadi698@gmail.com>
2026-06-20 09:08:26 +0530

    Toggle implemented

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,11 @@
 //alert("Hello From JS!");
 
 console.log("Hello from js!");
+
+const toggleBtn = document.getElementById("toggleBtn");
+
+if (toggleBtn) {
+    toggleBtn.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+}
